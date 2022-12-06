66fd8a06a35421dd33011fbd2325ac675d58d109
jwoon1013 <jwoon1013@gmail.com>
2022-12-06 16:47:39 +0900

    <완료> 메모사이트에 CRUD 기능 만들기 실습 완료

---
+++ src/main/java/com/sparta/springpracmemo/dto/MemoRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.springpracmemo.dto;
+
+public class MemoRequestDto {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    private String contents;
+}
---
+++ src/main/java/com/sparta/springpracmemo/entity/Memo.java
@@ -0,0 +1,31 @@
+package com.sparta.springpracmemo.entity;
+
+import com.sparta.springpracmemo.dto.MemoRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Memo extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    public void update(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
