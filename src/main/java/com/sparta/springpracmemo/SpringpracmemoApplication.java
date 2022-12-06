a6e38a81953112a444826d72027c59c41ee73ea0
jwoon1013 <jwoon1013@gmail.com>
2022-12-06 14:54:12 +0900

    <세팅>프로젝트 생성

---
+++ src/main/java/com/sparta/springpracmemo/SpringpracmemoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springpracmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracmemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpracmemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springpracmemo/SpringpracmemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpracmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpracmemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
